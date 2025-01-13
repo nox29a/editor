4be8a7d9db208cc34ad10b788021fd4a82fdc003
Łukasz <60299329+nox29a@users.noreply.github.com>
2025-01-13 14:58:25 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,11 +16,8 @@ const Header = () => {
 
             <div className='m-2 p-2 border-2 border-purple-800 bg-black rounded-xl bg-opacity-60 cursor-pointer' >
             <a href='#Showreel'>Showreel</a></div>
-            <div className='m-2 p-2 border-2 border-purple-800 bg-black rounded-xl bg-opacity-60 cursor-pointer' >
-            <a href='#Videos'>Films</a></div>
 
-            <div className='m-2 p-2 border-2 border-purple-800 bg-black rounded-xl bg-opacity-60 cursor-pointer' >
-            <a href='#Photos'>Photos</a></div>
+
             <div className='m-2 p-2 border-2 border-purple-800 bg-black rounded-xl bg-opacity-60 cursor-pointer' >
             <a href='#Socials'>Contact</a></div>
           </div>    
